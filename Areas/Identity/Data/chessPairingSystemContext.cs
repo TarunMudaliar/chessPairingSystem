e29cd457272c4336b13cb6060e19ca9236b4c920
Tarun Mudaliar <ac148909@avcol.school.nz>
2026-03-26 22:17:14 +1300

    Scaffolded Category controller and views.

--- Areas/Identity/Data/chessPairingSystemContext.cs
+++ Areas/Identity/Data/chessPairingSystemContext.cs
@@ -20,4 +20,6 @@ public class chessPairingSystemContext : IdentityDbContext<ApplicationUser>
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
     }
+
+public DbSet<chessPairingSystem.Models.Category> Category { get; set; } = default!;
 }
