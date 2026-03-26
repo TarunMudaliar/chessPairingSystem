3876c43c814d779612f447cb90c5d94cdb2f95cc
Tarun Mudaliar <ac148909@avcol.school.nz>
2026-03-26 18:45:18 +1300

    Created the model class for Category. Added data annotations to it aswell.

---
+++ Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace chessPairingSystem.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string CategoryName { get; set; }
+    }
+}
