e5c7851a9bd8aca7f84caa0d8f57bf4ef99c0b22
Tarun Mudaliar <ac148909@avcol.school.nz>
2026-03-28 11:26:32 +1300

    Added validations to Category.cs

--- Models/Category.cs
+++ Models/Category.cs
@@ -7,8 +7,10 @@ namespace chessPairingSystem.Models
         [Key]
         public int CategoryId { get; set; }
 
+        // Category name is required and has to be under 50 characters
         [Required]
         [StringLength(50)]
+        [Display(Name = "Category Name")]
         public string CategoryName { get; set; }
     }
 }
